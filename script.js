68ebd4e86341289eb9b52b33a1a7beef078bc272
Nikhil-Mohan-01 <127566613+Nikhil-Mohan-01@users.noreply.github.com>
2024-06-30 00:21:15 -0400

    Finished Polishing the website.

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ function opentab(tabName){
 }
 
 let slideIndex = 0;
+let slideTimeout;
+
 showSlides();
 
 function plusSlides(n) {
+    clearTimeout(slideTimeout);
     showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
+    clearTimeout(slideTimeout);
     showSlides(slideIndex = n);
 }
 
@@ -27,23 +31,30 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+    
     if (n === undefined) {
         slideIndex++;
     } else {
         slideIndex = n;
     }
+
     if (slideIndex > slides.length) {slideIndex = 1}
     if (slideIndex < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+
+    slideTimeout = setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
 
 var sideMenu = document.getElementById("sideMenu");
 
